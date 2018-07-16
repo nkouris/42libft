f086e7af789c0b96899316e071671e4b8f1850d4
nkouris <nkouris@student.42.us.org>
2018-07-15 17:57:49 -0700

    yep

--- src/ft_num/ft_nuitoa_base.c
+++ src/ft_num/ft_nuitoa_base.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 13:51:22 by nkouris           #+#    #+#             */
-/*   Updated: 2018/07/14 14:07:36 by nkouris          ###   ########.fr       */
+/*   Updated: 2018/07/15 17:57:36 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,10 @@ char		*ft_nuitoa_base(unsigned int n, int base, int length)
 	static char	result[3];
 
 	ft_bzero(result, 3);
-	printf("incoming : %u\t", n);
 	while (length)
 	{
 		result[--length] = BASEKEY[n % base];
-		printf("the char : %c\t", BASEKEY[n % base]);
 		n =  n / base;
 	}
-	printf("result : %s\n", result);
 	return (result);
 }
